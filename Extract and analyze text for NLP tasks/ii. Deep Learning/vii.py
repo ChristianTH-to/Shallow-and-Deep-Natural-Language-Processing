05a810171777319f60c6d71a8e1ccf3eb9048a97
Christian Hardy <ChristianTH.to@gmail.com>
2022-11-20 16:27:09 -0500

    Update vii.py

--- Extract and analyze text for NLP tasks/ii. Deep Learning/vii.py
+++ Extract and analyze text for NLP tasks/ii. Deep Learning/vii.py
@@ -1,23 +1,20 @@
 # Deciding if the classification problem should be modeled as binary or multi-class
 # with Evolutionary Data Measures
 
-
-# Dependencies
-
+# Stdlib
 import os 
+
+# Third party
+import pandas as pd
 from edm import report
 
-# Data
-
-import pandas as pd
+# Import data
 dataset = pd.read_csv('bt_train_data_evaluation_set.csv').fillna('')
 df = dataset.sample(frac=0.30)
 messages = df['Message'].fillna('_##_').values
 labels = df['Name'].values
 
-
 # Returns a set of metrics and statistics that says 
 # how likely it is that any algorithm we train can learn 
 # from the dataset
-
 print(report.get_difficulty_report(messages, labels))
