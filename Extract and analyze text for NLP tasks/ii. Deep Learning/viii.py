43daed58e0de699337723f93928ec2b26b34fe8f
Christian Hardy <ChristianTH.to@gmail.com>
2022-11-20 18:53:38 -0500

    Update viii.py

--- Extract and analyze text for NLP tasks/ii. Deep Learning/viii.py
+++ Extract and analyze text for NLP tasks/ii. Deep Learning/viii.py
@@ -150,75 +150,76 @@ def f2_score(y_true, y_pred):
     return tf.reduce_mean(f_score)
 
 
+''''
+def tp_score(y_true, y_pred, threshold=0.1):
+    tp_3d = K.concatenate(
+        [
+            K.cast(K.expand_dims(K.flatten(y_true)), 'bool'),
+            K.cast(K.expand_dims(K.flatten(K.greater(y_pred, K.constant(threshold)))), 'bool'),
+            K.cast(K.ones_like(K.expand_dims(K.flatten(y_pred))), 'bool')
+        ], axis=1
+    )
 
-#def tp_score(y_true, y_pred, threshold=0.1):
-#    tp_3d = K.concatenate(
-#        [
-#            K.cast(K.expand_dims(K.flatten(y_true)), 'bool'),
-#            K.cast(K.expand_dims(K.flatten(K.greater(y_pred, K.constant(threshold)))), 'bool'),
-#            K.cast(K.ones_like(K.expand_dims(K.flatten(y_pred))), 'bool')
-#        ], axis=1
-#    )
+    tp = K.sum(K.cast(K.all(tp_3d, axis=1), 'int32'))
 
-#    tp = K.sum(K.cast(K.all(tp_3d, axis=1), 'int32'))
-
-#    return tp
-
-
-#def fp_score(y_true, y_pred, threshold=0.1):
-#    fp_3d = K.concatenate(
-#        [
-#            K.cast(K.expand_dims(K.flatten(K.abs(y_true - K.ones_like(y_true)))), 'bool'),
-#            K.cast(K.expand_dims(K.flatten(K.greater(y_pred, K.constant(threshold)))), 'bool'),
-#            K.cast(K.ones_like(K.expand_dims(K.flatten(y_pred))), 'bool')
-#        ], axis=-1
-#    )
-
-#    fp = K.sum(K.cast(K.all(fp_3d, axis=1), 'int32'))
-
-#    return fp
+    return tp
 
 
-#def fn_score(y_true, y_pred, threshold=0.1):
-#    fn_3d = K.concatenate(
-#        [
-#            K.cast(K.expand_dims(K.flatten(y_true)), 'bool'),
-#            K.cast(K.expand_dims(K.flatten(K.abs(K.cast(K.greater(y_pred, K.constant(threshold)),
-#            	'float') - K.ones_like(y_pred)))), 'bool'),
-#            K.cast(K.ones_like(K.expand_dims(K.flatten(y_pred))), 'bool')
-#        ], axis=1
-#    )
+def fp_score(y_true, y_pred, threshold=0.1):
+    fp_3d = K.concatenate(
+        [
+            K.cast(K.expand_dims(K.flatten(K.abs(y_true - K.ones_like(y_true)))), 'bool'),
+            K.cast(K.expand_dims(K.flatten(K.greater(y_pred, K.constant(threshold)))), 'bool'),
+            K.cast(K.ones_like(K.expand_dims(K.flatten(y_pred))), 'bool')
+        ], axis=-1
+    )
 
-#    fn = K.sum(K.cast(K.all(fn_3d, axis=1), 'int32'))
+    fp = K.sum(K.cast(K.all(fp_3d, axis=1), 'int32'))
 
-#    return fn
+    return fp
 
-# Defines precision curve
 
-#def precision_score(y_true, y_pred, threshold=0.1):
-#    tp = tp_score(y_true, y_pred, threshold)
-#    fp = fp_score(y_true, y_pred, threshold)
+def fn_score(y_true, y_pred, threshold=0.1):
+    fn_3d = K.concatenate(
+        [
+            K.cast(K.expand_dims(K.flatten(y_true)), 'bool'),
+            K.cast(K.expand_dims(K.flatten(K.abs(K.cast(K.greater(y_pred, K.constant(threshold)),
+            	'float') - K.ones_like(y_pred)))), 'bool'),
+            K.cast(K.ones_like(K.expand_dims(K.flatten(y_pred))), 'bool')
+        ], axis=1
+    )
 
-#    return tp / (tp + fp)
+    fn = K.sum(K.cast(K.all(fn_3d, axis=1), 'int32'))
 
-# Defines recall curve
+    return fn
 
-#def recall_score(y_true, y_pred, threshold=0.1):
-#    tp = tp_score(y_true, y_pred, threshold)
-#    fn = fn_score(y_true, y_pred, threshold)
+ Defines precision curve
 
-#    return tp / (tp + fn)
+def precision_score(y_true, y_pred, threshold=0.1):
+    tp = tp_score(y_true, y_pred, threshold)
+    fp = fp_score(y_true, y_pred, threshold)
 
-# Defines f1 score
+    return tp / (tp + fp)
 
-#def f_score(y_true, y_pred, threshold=0.1, beta=2):
-#    tp = tp_score(y_true, y_pred, threshold)
-#    fp = fp_score(y_true, y_pred, threshold)
-#    fn = fn_score(y_true, y_pred, threshold)
+ Defines recall curve
 
-#    precision = tp / (tp + fp)
-#    recall = tp / (tp + fn)
-#    return (1+beta**2) * ((precision * recall) / ((beta**2)*precision + recall))
+def recall_score(y_true, y_pred, threshold=0.1):
+    tp = tp_score(y_true, y_pred, threshold)
+    fn = fn_score(y_true, y_pred, threshold)
+
+    return tp / (tp + fn)
+
+ Defines f1 score
+
+def f_score(y_true, y_pred, threshold=0.1, beta=2):
+    tp = tp_score(y_true, y_pred, threshold)
+    fp = fp_score(y_true, y_pred, threshold)
+    fn = fn_score(y_true, y_pred, threshold)
+
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    return (1+beta**2) * ((precision * recall) / ((beta**2)*precision + recall))
+''''
 
 
 # Import data
