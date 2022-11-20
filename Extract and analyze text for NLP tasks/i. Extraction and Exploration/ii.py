ff4269fe86394984c8f69a3e9d518de27a16d539
Christian Hardy <ChristianTH.to@gmail.com>
2022-11-20 15:53:50 -0500

    Update ii.py

--- Extract and analyze text for NLP tasks/i. Extraction and Exploration/ii.py
+++ Extract and analyze text for NLP tasks/i. Extraction and Exploration/ii.py
@@ -1,19 +1,22 @@
 # Zipf's Law
 
-# Data
+# Stdlib
+import re
 
+# Third party
 import pandas as pd
-df = pd.read_csv('bt_train_data.csv').fillna(' ')
+import matplotlib.pyplot as plt
+%matplotlib inline
 
+
+# Import data
+df = pd.read_csv('bt_train_data.csv').fillna(' ')
 df = df[['Message']]
 
 # Lower cases all words in df
-
-import re
 df['clean_text'] = df.Message.apply(lambda x: re.sub('[^A-Za-z\']', ' ', x.lower()))
 
 # Counts all words from data and scales them logarithmically
-
 word_list = ' '.join(df.clean_text.values).split(' ')
 words = pd.DataFrame(word_list, columns=['word'])
 word_counts = words.word.value_counts().reset_index()
@@ -21,9 +24,6 @@ word_counts.columns = ['word', 'log']
 word_counts['bt_2000_word_rank'] = word_counts.log.rank(ascending=False)
 
 # Plot Zipf's Law
-
-import matplotlib.pyplot as plt
-%matplotlib inline
 f, ax = plt.subplots(figsize=(12, 12))
 ax.set(xscale="log", yscale="log")
 import seaborn as sns
